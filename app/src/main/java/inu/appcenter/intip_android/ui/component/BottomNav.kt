81dd7108c94e1e9710605cc9033cbbc12405a1d3
바키찬 <ckstmznf@naver.com>
2025-01-06 19:19:31 +0900

    BottomItem 위에 Border 추가

--- app/src/main/java/inu/appcenter/intip_android/ui/component/BottomNav.kt
+++ app/src/main/java/inu/appcenter/intip_android/ui/component/BottomNav.kt
@@ -1,11 +1,13 @@
 package inu.appcenter.intip_android.ui.component
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
@@ -17,9 +19,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ModifierInfo
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -41,12 +46,15 @@ fun BottomNav() {
         modifier = Modifier
             .fillMaxWidth()
             .shadow(3.dp, shape)
-            .background(color = Color(0xffffffff), shape = shape)
-            .padding(vertical = 12.dp),
+            .background(color = Color(0xffffffff), shape = shape),
         horizontalArrangement = Arrangement.SpaceAround,
     ) {
         for (item in bottomSheetItem) {
-            BottomItem(icon = item.first, text = item.second.label, isSelected = false)
+            BottomItem(
+                icon = item.first,
+                text = item.second.label,
+                isSelected = item.second.route == Route.Home.route
+            )
         }
     }
 }
@@ -60,7 +68,23 @@ fun BottomItem(
     val tintColor = if (isSelected) Color(0xff4071B9) else Color(0xffD6D1D5)
 
     Column(
-        horizontalAlignment = Alignment.CenterHorizontally
+        modifier = Modifier
+            .drawBehind {
+                if(isSelected) {
+                    val strokeWidth = 4.dp.toPx()
+                    val color = Color(0xff4071B9)
+
+                    drawLine(
+                        color = color,
+                        start = Offset(x = 0f, y = 0f),
+                        end = Offset(size.width, y = 0f),
+                        strokeWidth = strokeWidth
+                    )
+                }
+            }
+            .padding(vertical = 8.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
         Icon(
             imageVector = icon,
