bb3259c4675e2cb64e2663dc9a580639a1b23778
Junhee-lee <sk5514224@gmail.com>
2025-01-15 17:49:19 +0900

    [feat] Complete infinity scroll and remove redline topbar innerpadding

---
+++ app/src/main/java/inu/appcenter/intip_android/ui/component/CustomAndroidView.kt
@@ -0,0 +1,73 @@
+package inu.appcenter.intip_android.ui.component
+
+import android.util.Log
+import android.view.ViewGroup
+import android.webkit.ConsoleMessage
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun CustomAndroidView(
+    paddingValue: PaddingValues,
+    path: String
+){
+    val WEB_BASE_URL = "https://intip.inuappcenter.kr/app"
+
+
+    AndroidView(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(),
+        factory = { context ->
+            WebView(context).apply {
+                // 레이아웃 파라미터 설정
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                // 디버깅 활성화
+                WebView.setWebContentsDebuggingEnabled(true)
+
+                // WebView 설정
+                settings.javaScriptEnabled = true
+                settings.domStorageEnabled = true
+                settings.useWideViewPort = true
+                settings.loadWithOverviewMode = true
+                settings.setSupportZoom(false)
+                settings.displayZoomControls = false
+
+
+
+
+                webChromeClient = object : WebChromeClient() {
+                    /**
+                     * 콘솔 메시지를 캡처하기 위해 오버라이드
+                     */
+                    /**
+                     * 콘솔 메시지를 캡처하기 위해 오버라이드
+                     */
+                    override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
+                        // 원하는 대로 처리(필터링, 로깅 등)
+                        Log.d(
+                            "WebViewConsole",
+                            "[${consoleMessage.messageLevel()}] " +
+                                    "${consoleMessage.message()} " +
+                                    "(line: ${consoleMessage.lineNumber()}, source: ${consoleMessage.sourceId()})"
+                        )
+                        return super.onConsoleMessage(consoleMessage)
+                    }
+                }
+                loadUrl("${WEB_BASE_URL}${path}")
+            }
+        },
+        update = { webView ->
+            webView.loadUrl("${WEB_BASE_URL}${path}")
+        }
+    )
+}
--- app/src/main/java/inu/appcenter/intip_android/ui/screen/WebViewScreen.kt
+++ app/src/main/java/inu/appcenter/intip_android/ui/screen/WebViewScreen.kt
@@ -1,24 +1,35 @@
+@file:Suppress("DEPRECATION")
+
 package inu.appcenter.intip_android.ui.screen
 
 import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import android.webkit.ConsoleMessage
 import android.webkit.WebChromeClient
 import android.webkit.WebSettings
 import android.webkit.WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.navigation.NavHostController
 import inu.appcenter.intip_android.ui.component.AppBottomBar
+import inu.appcenter.intip_android.ui.component.CustomAndroidView
 
 @Composable
 fun WebViewScreen(modifier: Modifier = Modifier, navController: NavHostController, path: String) {
@@ -29,50 +40,11 @@ fun WebViewScreen(modifier: Modifier = Modifier, navController: NavHostControlle
         bottomBar = {
             AppBottomBar(navController)
         }
-    ) {
-        AndroidView(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(it),
-            factory = { context ->
-                WebView(context).apply {
-                    // 디버깅 활성화
-                    WebView.setWebContentsDebuggingEnabled(true)
-
-                    // WebView 설정
-                    settings.javaScriptEnabled = true
-                    settings.domStorageEnabled = true
-                    settings.useWideViewPort = true
-                    settings.loadWithOverviewMode = true
-                    settings.setSupportZoom(true)
-                    //settings.builtInZoomControls = true
-                    settings.displayZoomControls = false
-                    settings.mixedContentMode = MIXED_CONTENT_ALWAYS_ALLOW
-                    settings.cacheMode = WebSettings.LOAD_DEFAULT
-
-                    webViewClient = WebViewClient()
-
-                    webChromeClient = object : WebChromeClient() {
-                        /**
-                         * 콘솔 메시지를 캡처하기 위해 오버라이드
-                         */
-                        override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
-                            // 원하는 대로 처리(필터링, 로깅 등)
-                            Log.d(
-                                "WebViewConsole",
-                                "[${consoleMessage.messageLevel()}] " +
-                                        "${consoleMessage.message()} " +
-                                        "(line: ${consoleMessage.lineNumber()}, source: ${consoleMessage.sourceId()})"
-                            )
-                            return super.onConsoleMessage(consoleMessage)
-                        }
-                    }
-                    loadUrl("${WEB_BASE_URL}${path}")
-                }
-            },
-            update = { webView ->
-                webView.loadUrl("${WEB_BASE_URL}${path}")
-            }
+    ) { paddingValue ->
+        CustomAndroidView(
+            paddingValue = paddingValue,
+            path = path
         )
+
     }
 }
