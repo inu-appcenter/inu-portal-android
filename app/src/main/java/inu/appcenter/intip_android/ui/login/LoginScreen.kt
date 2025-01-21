c8961a1142133b0c72d0e180f64612c57a588b21
Junhee-lee <sk5514224@gmail.com>
2025-01-21 17:20:10 +0900

    [feat] bottom bar label change to english

--- app/src/main/java/inu/appcenter/intip_android/ui/login/LoginScreen.kt
+++ app/src/main/java/inu/appcenter/intip_android/ui/login/LoginScreen.kt
@@ -2,7 +2,6 @@ package inu.appcenter.intip_android.ui.login
 
 import android.content.Intent
 import android.net.Uri
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -23,7 +22,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
--- app/src/main/java/inu/appcenter/intip_android/ui/navigate/AllDestination.kt
+++ app/src/main/java/inu/appcenter/intip_android/ui/navigate/AllDestination.kt
@@ -1,9 +1,9 @@
 package inu.appcenter.intip_android.ui.navigate
 
 sealed class AllDestination(val route: String, val label: String, val webPath : String? = null) {
-    data object Main : AllDestination("main", "홈", "/home")
-    data object Save : AllDestination("save", "저장소", "/save")
-    data object Write : AllDestination("write", "작성", "/write")
-    data object MyPage : AllDestination("myPage", "마이페이지", "/mypage")
+    data object Main : AllDestination("main", "Home", "/home")
+    data object Save : AllDestination("save", "Save", "/save")
+    data object Write : AllDestination("write", "Write", "/write")
+    data object MyPage : AllDestination("myPage", "Mypage", "/mypage")
     data object Login : AllDestination("login", "로그인")
 }
