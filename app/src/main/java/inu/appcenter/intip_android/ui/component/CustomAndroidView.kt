9cbfb8aecf3f0a78d3e6764e0353db2d36532af5
Junhee-lee <sk5514224@gmail.com>
2025-01-23 23:35:42 +0900

    [feat] extraheader에서 url 쿼리 파라미터로 토큰 전달 방식 변경

--- app/src/main/java/inu/appcenter/intip_android/ui/component/CustomAndroidView.kt
+++ app/src/main/java/inu/appcenter/intip_android/ui/component/CustomAndroidView.kt
@@ -1,5 +1,6 @@
 package inu.appcenter.intip_android.ui.component
 
+import android.net.Uri
 import android.util.Log
 import android.view.ViewGroup
 import android.webkit.ConsoleMessage
@@ -22,43 +23,39 @@ import androidx.compose.ui.viewinterop.AndroidView
 fun CustomAndroidView(
     modifier: Modifier = Modifier,
     path: String,
-    token: String? = null  // 추가: 토큰
+    token: String? = null
 ) {
-    val context = LocalContext.current  // Composable 함수 외부에서 context를 얻음
+    val context = LocalContext.current
     val WEB_BASE_URL = "https://intip.inuappcenter.kr/app"
-    val url = "$WEB_BASE_URL$path"
+
+    val url = token?.let {
+        val baseUri = Uri.parse("$WEB_BASE_URL$path")
+        val urlBuilder = baseUri.buildUpon()
+        urlBuilder.appendQueryParameter("token", it)
+        urlBuilder.toString()
+    } ?: "$WEB_BASE_URL$path"
 
     val webView = remember { WebView(context) }
 
-    // 토큰이 있는 경우, extra headers를 구성합니다.
-//    val headers = if (!token.isNullOrEmpty()) {
-//        mapOf("Auth" to "$token")
-//    } else {
-//        emptyMap()
-//    }
-
-    // Initialize the WebView only once
     DisposableEffect(webView) {
         webView.apply {
-            // 레이아웃 파라미터 설정
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
-            // 디버깅 활성화
             WebView.setWebContentsDebuggingEnabled(true)
 
-            // WebView 설정
-            settings.javaScriptEnabled = true
-            settings.domStorageEnabled = true
-            settings.useWideViewPort = true
-            settings.loadWithOverviewMode = true
-            settings.setSupportZoom(false)
-            settings.displayZoomControls = false
+            settings.apply {
+                javaScriptEnabled = true
+                domStorageEnabled = true
+                useWideViewPort = true
+                loadWithOverviewMode = true
+                setSupportZoom(false)
+                displayZoomControls = false
+            }
 
             webChromeClient = object : WebChromeClient() {
                 override fun onConsoleMessage(consoleMessage: ConsoleMessage): Boolean {
-                    // 원하는 대로 처리 (필터링, 로깅 등)
                     Log.d(
                         "WebViewConsole",
                         "[${consoleMessage.messageLevel()}] " +
@@ -76,15 +73,19 @@ fun CustomAndroidView(
                 ): WebResourceResponse? {
                     request?.let {
                         Log.d("WebViewRequest", "URL: ${it.url}")
-                        it.requestHeaders.forEach { header ->
-                            Log.d("WebViewRequest", "${header.key}: ${header.value}")
-                        }
                     }
                     return super.shouldInterceptRequest(view, request)
                 }
-            }
 
-            // 초기 로딩은 하지 않습니다.
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    token?.let {
+                        view?.evaluateJavascript(
+                            "localStorage.setItem('accessToken', '$it');",
+                            null
+                        )
+                    }
+                }
+            }
         }
 
         onDispose {
@@ -93,19 +94,11 @@ fun CustomAndroidView(
     }
 
     AndroidView(
-        modifier = modifier
-            .fillMaxSize()
-            .padding(),
+        modifier = modifier.fillMaxSize().padding(),
         factory = { webView },
         update = { view ->
-            if (!token.isNullOrEmpty()) {
-                val headers = mapOf("Auth" to token)
-                Log.d("WebViewUpdate", "Updating URL with headers: $headers")
-                view.loadUrl(url, headers)
-            } else {
-                Log.d("WebViewUpdate", "Updating URL without headers")
-                view.loadUrl(url)
-            }
+            Log.d("WebViewUpdate", "Loading URL: $url")
+            view.loadUrl(url)
         }
     )
 }
