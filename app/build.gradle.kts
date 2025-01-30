ec426f10d8ee072c5421d7ef6cd78528fbb56108
바키찬 <ckstmznf@naver.com>
2025-01-30 23:10:38 +0900

    버전 코드 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "inu.appcenter.intip_android"
         minSdk = 25
         targetSdk = 34
-        versionCode = 2
-        versionName = "0.0.2"
+        versionCode = 3
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
