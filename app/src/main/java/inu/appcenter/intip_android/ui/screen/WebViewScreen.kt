5cc27a4086b20dc7f8c00ceff9ab4cf1d209599a
Junhee-lee <sk5514224@gmail.com>
2025-02-07 12:41:10 +0900

    [feat] 상단바 흰색 배경으로 분리해서 설정

--- app/src/main/java/inu/appcenter/intip_android/ui/screen/WebViewScreen.kt
+++ app/src/main/java/inu/appcenter/intip_android/ui/screen/WebViewScreen.kt
@@ -1,14 +1,22 @@
 package inu.appcenter.intip_android.ui.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBars
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -28,6 +36,16 @@ fun WebViewScreen(
     val uiState by authViewModel.uiState.collectAsState()
 
     Scaffold(
+        topBar = {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    // WindowInsets.statusBars.asPaddingValues()로 상태바의 패딩 값을 구한 후, calculateTopPadding()으로 높이를 가져옴
+                    // 4dp 추가한 이유는 상단의 공백과 동일하게 하단의 공백을 맞추기 위해서 추가함
+                    .height(WindowInsets.statusBars.asPaddingValues().calculateTopPadding() + 4.dp)
+                    .background(Color.White)
+            )
+        },
         bottomBar = {
             if (isShowBottomBar) {
                 AppBottomBar(navController)
@@ -38,7 +56,7 @@ fun WebViewScreen(
             PaddingValues(
                 start = paddingValue.calculateStartPadding(LayoutDirection.Ltr),
                 end = paddingValue.calculateEndPadding(LayoutDirection.Rtl),
-                top = 0.dp,
+                top = paddingValue.calculateTopPadding(),
                 // Scafflod의 기본 BottomPadding에서 BottomNavigation의 둥근 정도를 뺀 값
                 bottom = paddingValue.calculateBottomPadding() - K.BOTTOM_BAR_OFFSET
             )
@@ -46,7 +64,7 @@ fun WebViewScreen(
             PaddingValues(
                 start = paddingValue.calculateStartPadding(LayoutDirection.Ltr),
                 end = paddingValue.calculateEndPadding(LayoutDirection.Rtl),
-                top = 0.dp,  // 상태바 영역의 패딩을 제거
+                top = paddingValue.calculateTopPadding(),
                 bottom = paddingValue.calculateBottomPadding()
             )
         }
