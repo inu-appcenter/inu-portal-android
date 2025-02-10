d33c031fb5b9e5b7a3a7685025de3f40da212df0
Junhee-lee <sk5514224@gmail.com>
2025-02-10 16:43:26 +0900

    [feat] version upgrade 0.0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "inu.appcenter.intip_android"
         minSdk = 25
         targetSdk = 34
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 6
+        versionName = "0.0.6"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
