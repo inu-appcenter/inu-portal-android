7f802143298e0c0a0b273becdd745570b0fa03ee
바키찬 <ckstmznf@naver.com>
2025-02-11 23:32:17 +0900

    버전 업그레이드 0.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "inu.appcenter.intip_android"
         minSdk = 25
         targetSdk = 34
-        versionCode = 8
-        versionName = "0.0.8"
+        versionCode = 9
+        versionName = "0.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
