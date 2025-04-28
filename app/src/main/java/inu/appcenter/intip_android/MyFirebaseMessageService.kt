acb5275d31917d1a382a12b6bafd3bb155f86685
바키찬 <ckstmznf@naver.com>
2025-04-28 16:58:31 +0900

    Feat: 알림 코드 정리

--- app/src/main/java/inu/appcenter/intip_android/MyFirebaseMessageService.kt
+++ app/src/main/java/inu/appcenter/intip_android/MyFirebaseMessageService.kt
@@ -1,24 +1,9 @@
 package inu.appcenter.intip_android
 
-import android.Manifest
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
-import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
-import android.util.Log
-import androidx.annotation.RequiresApi
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
-import kotlin.random.Random
 
 class MyFirebaseMessageService : FirebaseMessagingService() {
-
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
 
@@ -29,49 +14,4 @@ class MyFirebaseMessageService : FirebaseMessagingService() {
             this.showNotification(title, body)
         }
     }
-}
-
-//todo: 미래의 내가 코드를 정리할거야
-private val CHANNEL_ID = "alert"
-private fun createChannel(context: Context) {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val channel = NotificationChannel(
-            CHANNEL_ID,
-            "일반 알림",
-            NotificationManager.IMPORTANCE_DEFAULT
-        ).apply {
-            description = "앱에서 오는 기본 알림"
-        }
-        context.getSystemService(NotificationManager::class.java)
-            .createNotificationChannel(channel)
-    }
-}
-private fun Context.showNotification(title: String, body: String) {
-    createChannel(this)
-
-    val intent = Intent(this, MainActivity::class.java).apply {
-        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TOP
-    }
-    val pending = PendingIntent.getActivity(
-        this, 0, intent,
-        PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
-    )
-
-    val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-        .setSmallIcon(R.drawable.icon_home_active)
-        .setContentTitle(title)
-        .setContentText(body)
-        .setAutoCancel(true)
-        .setContentIntent(pending)
-        .setPriority(NotificationCompat.PRIORITY_MAX)
-        .build()
-
-    // 4) 뿌리기
-    if (ActivityCompat.checkSelfPermission(this,
-            Manifest.permission.POST_NOTIFICATIONS
-        ) != PackageManager.PERMISSION_GRANTED
-    ) {
-        Log.e("Notification", "알림 권한 없음")
-    }
-    NotificationManagerCompat.from(this).notify(Random.nextInt(), notification)
 }
